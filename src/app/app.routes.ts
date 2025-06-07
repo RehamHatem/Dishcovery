5ab787d2b865f877ce25aa4c31f8ad4e6f100a35
reham hatem <rha772201@gmail.com>
2025-06-07 18:22:28 +0300

    not found component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
+import { NotFoundComponent } from './shared/not-found/not-found.component';
 
 export const routes: Routes = [
+    {path:"",redirectTo: 'dishcovery', pathMatch: 'full' },
     {path:"dishcovery",component:RecipesListComponent},
+    {path:'**',component:NotFoundComponent}
 ];
