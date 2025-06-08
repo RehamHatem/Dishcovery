a082a8b0d044772b4a00c38de90ad8dabb44887a
reham hatem <rha772201@gmail.com>
2025-06-08 05:05:08 +0300

    the same but dont change the recipes list

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -3,48 +3,21 @@ import { RecipesService } from '../../core/recipes.service';
 import { CommonModule } from '@angular/common';
 import { MealCardComponent } from '../../shared/meal-card/meal-card.component';
 import { HttpClientModule } from '@angular/common/http';
-import { AreaServicesService, Meal } from '../../core/area-services.service';
-import { AreaListComponent } from '../../area/area-list/area-list.component';
-import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-recipes-list',
   standalone: true,
-  imports: [CommonModule,MealCardComponent, HttpClientModule,AreaListComponent],
+  imports: [CommonModule,MealCardComponent, HttpClientModule],
   templateUrl: './recipes-list.component.html',
   styleUrl: './recipes-list.component.css'
 })
 export class RecipesListComponent implements OnInit {
-  meals: Meal[] = [];
-  loading = false;
-  error: string | null = null;
-
-  constructor(private recipesService: RecipesService,private areaService:AreaServicesService,private route: ActivatedRoute) {}
-
+  meals:any[]=[];
+constructor(private recipesService:RecipesService){}
   ngOnInit() {
-this.route.queryParams.subscribe(params => {
-      const area = params['area'];
-      if (area) {
-        this.loadMealsByArea(area);
-      }
-    });
-
-  }
-
-  private loadMealsByArea(area: string) {
-    this.loading = true;
-    this.error = null;
-    this.areaService.getMealsByArea(area).subscribe({
-      next: (meals) => {
-        this.meals = meals;
-        this.loading = false;
-      },
-      error: (err) => {
-        this.error = err.message;
-        this.meals = [];
-        this.loading = false;
-      }
+    this.recipesService.getAllMeals().subscribe(data => {
+      this.meals = data;
     });
   }
-
+  
 }
