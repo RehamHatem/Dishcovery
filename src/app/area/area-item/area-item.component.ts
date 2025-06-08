4a69e002032338497f021743cdf1757e75249d19
reham hatem <rha772201@gmail.com>
2025-06-08 03:48:05 +0300

    disply areas and get meals by area

---
+++ src/app/area/area-item/area-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-area-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './area-item.component.html',
+  styleUrl: './area-item.component.css'
+})
+export class AreaItemComponent {
+
+}
--- src/app/core/area-services.service.spec.ts
+++ src/app/core/area-services.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
-import { RecipesService } from './recipes.service';
 
+import { AreaServicesService } from './area-services.service';
 
-describe('RecipesService', () => {
-  let service: RecipesService;
+describe('AreaServicesService', () => {
+  let service: AreaServicesService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(RecipesService);
+    service = TestBed.inject(AreaServicesService);
   });
 
   it('should be created', () => {
